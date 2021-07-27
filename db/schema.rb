86534ed65dfdeeba07d9cba7f5b9ba2d03ef719e
guchi3f4 <guchi3f4@gmail.com>
2021-07-27 09:46:19 +0000

    4-4

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_054622) do
+ActiveRecord::Schema.define(version: 2021_07_27_085412) do
 
   create_table "post_coments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
     t.integer "post_image"
