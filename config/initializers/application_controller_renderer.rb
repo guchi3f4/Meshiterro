299fb687b3ff37eb8db2df220ce46a021f05dbb2
guchi3f4 <guchi3f4@gmail.com>
2021-07-27 03:48:49 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '07310521e690a810fb506cc58405b915e4789b8806bad5f79f732a62047b7fa2a04e6749444e2b553bbdc353de14013d76051955501daf9acd1847da51dda9c0'
