712b72077a20a4c81c9515265113b7d729d4bba1
guchi3f4 <guchi3f4@gmail.com>
2021-07-30 09:14:00 +0000

    [Finish]4-5

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images.page(params[:page]).reverse_order
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(params_user)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+  def params_user
+    params.require(:user).permit(:name, :profile_image)
+  end
+end
